d8ab648694ba10ad86419c0eeecc9d09cfc4da5b
aidecoder <alfadecoder@gmail.com>
2018-04-24 00:18:21 +0300

    git init git add README.md git commit -m "first commit" git remote add origin https://github.com/aidecoder/ids.git git push -u origin master

---
+++ OnlineApi/Controllers/IdentityController.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [HttpGet]
+        [Route("superpowers2")]
+        [Authorize(Policy = "AdminsOnly")]
+        public IActionResult Superpowers2()
+        {
+            return new JsonResult("Superpowers!");
+        }
+    }
+}
---
+++ OnlineApi/Startup.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                // задаём политику CORS, чтобы наше клиентское приложение могло отправить запрос на сервер API
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins(
+                        "http://localhost:5593",
+                        "http://localhost:5592")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+            services.AddMvcCore()
+                .AddAuthorization(options =>
+                // политики позволяют не работать с Roles magic strings, содержащими перечисления ролей через запятую
+                options.AddPolicy("AdminsOnly", policyUser =>
+                {
+                    policyUser.RequireClaim("role", "admin");
+                })
+                )
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5590";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ApiName = "api1";
+                    options.RoleClaimType = "role";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            /*
+            app.UseCors(policy =>
+            {
+                policy.WithOrigins(
+                    "http://localhost:5593",
+                    "http://localhost:5592");
+
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.WithExposedHeaders("WWW-Authenticate");
+            });
+            */
+            app.UseCors("default");
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
